6af3b1b300ff8ac4e776b0ffae86ea1252fbc044
Domino55555 <doomion555@gmail.com>
2025-11-14 23:08:18 +0000

    Wave enemies spawn around the block instead of on the block

--- src/main/java/com/hellwaves/hellwavesmod/Wave.java
+++ src/main/java/com/hellwaves/hellwavesmod/Wave.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.Heightmap;
 
 import java.util.List;
 
@@ -15,13 +16,21 @@ public class Wave {
     }
 
     public void spawn(Level world, BlockPos pos) {
-        int i=0;
-        for (EntityType<? extends Mob> type : enemies) {
+        int numberofMobs = enemies.size();
+        double radius = 20.0;
+
+        for (int i = 0; i < numberofMobs; i++) {
+            EntityType<? extends Mob> type = enemies.get(i);
             Mob mob = type.create(world);
             if (mob != null) {
-                mob.moveTo(pos.getX() + i, pos.getY(), pos.getZ() + i, 0, 0);
+                double angle = 2 * Math.PI * i / numberofMobs;
+                double x = pos.getX() + radius * Math.cos(angle);
+                double z = pos.getZ() + radius * Math.sin(angle);
+
+                int y = world.getHeight(Heightmap.Types.MOTION_BLOCKING, (int)x, (int)z);
+
+                mob.moveTo(x, y, z, 0, 0);
                 world.addFreshEntity(mob);
-                i++;
             }
         }
     }
